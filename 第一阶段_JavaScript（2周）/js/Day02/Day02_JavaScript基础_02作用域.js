0b371471e1b743efe3df1ad8923f14f1e0a1421a
liangjunke <liangjunke198909@outlook.com>
2024-08-01 15:41:42 +0800

    LJK_StudyWeb_Day02_JavaScript_JS提高_20240801_重点更改5

--- 第一阶段_JavaScript（2周）/js/Day02/Day02_JavaScript基础_02作用域.js
+++ 第一阶段_JavaScript（2周）/js/Day02/Day02_JavaScript基础_02作用域.js
@@ -33,4 +33,31 @@ window.onload = function () {
         fn(); //undefined   20  30
         console.log(a); //10
     }
+    console.log("-----------------4、基础用法四------------------------")
+    {
+        var a = 10;
+        function fn() {
+            console.log(a);
+            a = 20;
+            console.log(a)
+            var a;
+            console.log(a);
+        }
+        fn(); //undefined   20  20
+        console.log(a); //10
+    }
+    console.log("-----------------5、基础用法五------------------------")
+    {
+        var a = 10;
+        function fn() {
+            var b=2*a;
+            var a=20;
+            var c=a+1;
+            console.log(b);
+            console.log(a);
+            console.log(c)
+        }
+        fn(); //NAN  20   21
+        console.log(a); //10
+    }
 }
