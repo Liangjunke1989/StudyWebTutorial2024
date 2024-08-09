75e2c3c544cb98b2e518e8eb8dea4339da1c1dfc
liangjunke <liangjunke198909@outlook.com>
2024-08-09 15:36:49 +0800

    20240809_JS基础_Day02_知识点四

--- 第一阶段_JavaScript（2周）/js/Day02/Day02_JavaScript基础_04apply、call、bind.js
+++ 第一阶段_JavaScript（2周）/js/Day02/Day02_JavaScript基础_04apply、call、bind.js
@@ -1,6 +1,7 @@
 window.onload = function () {
     console.log("--------------------01、call、apply、bind------------------------------")
     {
+        console.log("--------------------01、call、apply、bind区别------------------------------")
         var item = {
             id: (Math.random().toFixed(2)) * 100,
             name: '小明',
@@ -11,12 +12,17 @@ window.onload = function () {
             console.log(this.age);
             console.log(this.id);
         }
-
+        var person={
+            id:100,
+            action:fn1
+        }
         //call、apply、bind区别？
-        //this指向为item
-        fn1.apply(item);
-        fn1.call(item);
-        fn1.bind(item)();
 
+        //this指向为item
+        // fn1.apply(item);
+        // fn1.call(item);
+        // fn1.bind(item)();
+        person.action();
+        person.action.apply(item);
     }
 }
