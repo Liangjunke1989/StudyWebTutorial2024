187f00e01eb8ce1b9dc58b7a82b370209b741feb
liangjunke <liangjunke198909@outlook.com>
2025-02-20 08:41:18 +0800

    20250220_LJK_原型多种继承方式

--- 第一阶段_JavaScript（1周）/js/Day04/Day04_JavaScript基础_02原型继承方式.js
+++ 第一阶段_JavaScript（1周）/js/Day04/Day04_JavaScript基础_02原型继承方式.js
@@ -5,6 +5,7 @@ window.onload = function () {
             this.name = name;
             this.age = age;
         }
+
         Person.prototype.say = function () {
             console.log(this.name + ":" + this.age);
         }//原型方法
@@ -12,6 +13,7 @@ window.onload = function () {
             Person.call(this, name, age);
             this.score = score;
         }
+
         Student.prototype = new Person();//继承
         Student.prototype.constructor = Student;//重置构造函数
         Student.prototype.say = function () {
